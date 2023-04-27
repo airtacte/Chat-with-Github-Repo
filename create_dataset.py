a8e18e4f427432c1a91580e45c6f9a1221fe06d4
Edmund Berkmann <eddieberkmann@gmail.com>
2023-04-27 20:09:31 +0100

    setup part 2

--- create_dataset.py
+++ create_dataset.py
@@ -6,5 +6,27 @@ os.environ['ACTIVELOOP_TOKEN'] = "eyJhbGciOiJIUzUxMiIsImlhdCI6MTY4MjQ1NTc1OCwiZX
 
 import deeplake
 
-ds = deeplake.empty('hub://Steelo/Steelo-dataset')
 ds = deeplake.dataset('hub://Steelo/Steelo-dataset')
+
+
+
+
+deeplake_path = 'hub://edmundberkmann/dataset_name' 
+
+# Define an empty Deep Lake dataset 
+ds = deeplake.empty(deeplake_path, token = <your_api_token>) 
+
+with ds: 
+	# Define tensors in the Deep Lake dataset 
+	ds.create_tensor('tensor_1', htype = 'htype_1', sample_compression = 'compression_1') 
+	ds.create_tensor('tensor_2', htype = 'htype_2', sample_compression = 'compression_2') 
+	ds.create_tensor('tensor_3', htype = 'htype_3', sample_compression = 'compression_3') 
+
+	# Iterate through the primary data (typically a list of files, like images) 
+	for file in files: 
+		metadata_1, metadata_2 = get_metadata_for_file(file) # Find the metadata correspoding to the primary data 
+
+		# Append all data as a row(sample) in the Deep Lake dataset 
+		ds.append({'tensor_1': deeplake.read(file),
+		 'tensor_2': metadata_1,
+		 'tensor_3': metadata_2})
